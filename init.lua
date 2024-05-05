c587ceb45d5e03af23ba9eb7615d0c2039a3743e
Sam <livaco@livaco.dev>
2024-05-05 17:40:52 +0100

    get started, get lazy.nvim, color scheme and basic options

---
+++ init.lua
@@ -0,0 +1,12 @@
+require("plugins")
+
+-- Vim settings
+vim.opt.relativenumber = true
+vim.opt.number = false
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.clipboard = "unnamedplus"
+vim.opt.background = "dark" -- set this to dark or light
+vim.cmd("colorscheme oxocarbon")
