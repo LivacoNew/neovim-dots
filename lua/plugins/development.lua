ae6bec8577300b65f799c104d1a8cf6d26539667
Sam <livaco@livaco.dev>
2024-05-05 22:11:03 +0100

    add telescope

--- lua/plugins/development.lua
+++ lua/plugins/development.lua
@@ -24,4 +24,17 @@ return {
             })
         end
     },
+
+    -- Telescope, for fuzzy finding
+    -- https://github.com/nvim-telescope/telescope.nvim
+    {
+        'nvim-telescope/telescope.nvim', tag = '0.1.6',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+        keys = {
+            { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find File"},
+            { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Fuzzy Find"},
+            { "<leader>fb", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Fuzzy Find in Buffer"},
+            { "<leader>fv", "<cmd>Telescope buffers<cr>", desc = "Find Buffer"},
+        }
+    }
 }
