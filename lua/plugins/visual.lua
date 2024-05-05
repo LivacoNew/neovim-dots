e004ed653688a6e22c5bebe8c15082d6c3598c39
Sam <livaco@livaco.dev>
2024-05-05 22:39:36 +0100

    remove nvimtree's auto close, as it was breaking and causing infinite loops

--- lua/plugins/visual.lua
+++ lua/plugins/visual.lua
@@ -45,21 +45,8 @@ return {
             vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = function(data)
                 -- Only auto-open if we're asking to edit a directory 
                 if vim.fn.isdirectory(data.file) == 0 then return end 
-                vim.g.nvimtree_auto_close = false -- Prevent nvimtree from auto-closing itself when it's the last buffer, as currently it will be the only buffer
                 require("nvim-tree.api").tree.open() 
             end })
-            vim.api.nvim_create_autocmd("BufEnter", {
-                nested = true,
-                callback = function()
-                    if not vim.g.nvimtree_auto_close then 
-                        vim.g.nvimtree_auto_close = true
-                        return
-                    end 
-                    if #vim.api.nvim_list_wins() == 1 and require("nvim-tree.utils").is_nvim_tree_buf() then
-                        vim.cmd "quit"
-                    end
-                end
-            })
         end,
         keys = {
             { "<leader>e", "<cmd>NvimTreeFocus<cr>", desc = "Focus Nvim Tree" },
