69dc61c791ea82398162560d5a73c2be4f531664
Sam <livaco@livaco.dev>
2025-01-30 09:57:32 +0000

    fix my god awful luasnip config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -56,32 +56,46 @@ return {
     -- https://github.com/L3MON4D3/LuaSnip
     {
         "L3MON4D3/LuaSnip",
-        -- follow latest release.
-        version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-        -- install jsregexp (optional!).
-        build = "make install_jsregexp"
+        version = "v2.*",
+        build = "make install_jsregexp",
+        dependencies = { "rafamadriz/friendly-snippets" },
+        config = function()
+            local ls = require("luasnip")
+            ls.filetype_extend("php", { "html" })
+
+            vim.keymap.set({"i", "s"}, "<C-K>", function() ls.expand() end)
+            vim.keymap.set({"i", "s"}, "<C-L>", function() ls.jump( 1) end)
+            vim.keymap.set({"i", "s"}, "<C-J>", function() ls.jump(-1) end)
+            vim.keymap.set({"i", "s"}, "<C-E>", function()
+                if ls.choice_active() then
+                    ls.change_choice(1)
+                end
+            end)
+
+            require("luasnip.loaders.from_vscode").lazy_load()
+        end
     },
 
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		config = function()
-			local configs = require("nvim-treesitter.configs")
-			configs.setup({
-				highlight = {
-					enable = true,
-				},
-			})
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            local configs = require("nvim-treesitter.configs")
+            configs.setup({
+                highlight = {
+                    enable = true,
+                },
+            })
 
-			local parsers = require("nvim-treesitter.parsers").get_parser_configs()
-			parsers.blade = {
-				install_info = {
-					url = "https://github.com/EmranMR/tree-sitter-blade",
-					files = {"src/parser.c"},
-					branch = "main",
-				},
-				filetype = "blade"
-			}
-		end
-	}
+            local parsers = require("nvim-treesitter.parsers").get_parser_configs()
+            parsers.blade = {
+                install_info = {
+                    url = "https://github.com/EmranMR/tree-sitter-blade",
+                    files = {"src/parser.c"},
+                    branch = "main",
+                },
+                filetype = "blade"
+            }
+        end
+    }
 }
