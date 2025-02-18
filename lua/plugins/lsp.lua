fd384fd7a8bcba7ce2258d82f006a5a3e376138d
Sam <livaco@livaco.dev>
2025-02-18 21:53:14 +0000

    add hover.nvim and beacon.nvim

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -97,5 +97,30 @@ return {
                 filetype = "blade"
             }
         end
+    },
+
+    {
+        "lewis6991/hover.nvim",
+        config = function()
+            require("hover").setup({
+                init = function()
+                    require("hover.providers.lsp")
+                end,
+                preview_opts = {
+                    border = 'single'
+                },
+                preview_window = false,
+                title = true,
+                mouse_providers = {
+                    'LSP'
+                },
+                mouse_delay = 1000
+            })
+        end,
+        keys = {
+            { "<leader>lh", function()
+                require("hover").hover()
+            end, desc = "LSP Hover" },
+        }
     }
 }
